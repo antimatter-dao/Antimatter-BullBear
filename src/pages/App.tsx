2b905593adef61f0e548d4d3d7f532a09771f86d
iv172 <iv172m@hotmail.com>
2021-04-06 20:29:36 +0800

    implement info page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -19,22 +19,23 @@ import {
   RedirectToAddLiquidity
 } from './AddLiquidity/redirects'
 import MatterToken from './MatterToken'
-import MigrateV1 from './MigrateV1'
-import MigrateV1Exchange from './MigrateV1/MigrateV1Exchange'
+// import MigrateV1 from './MigrateV1'
+// import MigrateV1Exchange from './MigrateV1/MigrateV1Exchange'
 import RemoveV1Exchange from './MigrateV1/RemoveV1Exchange'
 import Pool from './Pool'
 import PoolFinder from './PoolFinder'
 import RemoveLiquidity from './RemoveLiquidity'
 import { RedirectOldRemoveLiquidityPathStructure } from './RemoveLiquidity/redirects'
 import Swap from './Swap'
-import { OpenClaimAddressModalAndRedirectToSwap, RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects'
-import Vote from './Vote'
-import VotePage from './Vote/VotePage'
+import { /*OpenClaimAddressModalAndRedirectToSwap,*/ RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects'
+// import Vote from './Vote'
+// import VotePage from './Vote/VotePage'
 import MarketStrategy from './MarketStrategy'
 import Generate from './Generate'
 import Redeem from './Redeem'
 import Exercise from './Exercise'
 import ComingSoon from './ComingSoon'
+import Info from './Info'
 
 const AppWrapper = styled.div`
   display: flex;
@@ -111,15 +112,15 @@ export default function App() {
                 <Route exact strict path="/redeem" component={Redeem} />
                 <Route exact strict path="/redeem/:currencyIdA/:currencyIdB" component={Redeem} />
                 <Route exact strict path="/governance" component={ComingSoon} />
-                <Route exact strict path="/info" component={ComingSoon} />
+                <Route exact strict path="/info" component={Info} />
                 <Route exact strict path="/exercise" component={Exercise} />
-                <Route exact strict path="/claim" component={OpenClaimAddressModalAndRedirectToSwap} />
+                {/* <Route exact strict path="/claim" component={OpenClaimAddressModalAndRedirectToSwap} /> */}
                 <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap} />
                 <Route exact strict path="/send" component={RedirectPathToSwapOnly} />
                 <Route exact strict path="/find" component={PoolFinder} />
                 <Route exact strict path="/liquidity" component={Pool} />
                 <Route exact strict path="/matter_token" component={MatterToken} />
-                <Route exact strict path="/vote" component={Vote} />
+                {/* <Route exact strict path="/vote" component={Vote} /> */}
                 <Route exact strict path="/create" component={RedirectToAddLiquidity} />
                 <Route exact path="/add" component={AddLiquidity} />
                 <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
@@ -130,9 +131,9 @@ export default function App() {
                 <Route exact strict path="/remove/v1/:address" component={RemoveV1Exchange} />
                 <Route exact strict path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
                 <Route exact strict path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
-                <Route exact strict path="/migrate/v1" component={MigrateV1} />
-                <Route exact strict path="/migrate/v1/:address" component={MigrateV1Exchange} />
-                <Route exact strict path="/vote/:id" component={VotePage} />
+                {/* <Route exact strict path="/migrate/v1" component={MigrateV1} />
+                <Route exact strict path="/migrate/v1/:address" component={MigrateV1Exchange} /> */}
+                {/* <Route exact strict path="/vote/:id" component={VotePage} /> */}
                 <Route component={RedirectPathToSwapOnly} />
               </Switch>
             </Web3ReactManager>
