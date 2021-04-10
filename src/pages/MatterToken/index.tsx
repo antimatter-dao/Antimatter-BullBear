49071ea4528cc05527d3b8e011ad18233106111a
iv172 <iv172m@hotmail.com>
2021-04-10 20:17:34 +0800

    restyle matter token manage modal

--- src/pages/MatterToken/index.tsx
+++ src/pages/MatterToken/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { AutoColumn } from 'components/Column'
+import styled from 'styled-components'
 import { LPT_TYPE } from 'constants/matterToken/matterTokenTokens'
 import MatterTokenManageModal from './MatterTokenManageModal'
 
+const Wrapper = styled.div`
+  grid-template-rows: auto;
+  grid-template-columns: 50% 50%;
+  grid-gap: 20px;
+  display: grid;
+  ${({ theme }) => theme.mediaWidth.upToMedium`  grid-template-columns: 100%;`}
+`
 export default function MatterToken() {
   return (
-    <AutoColumn gap="4rem">
+    <Wrapper>
       {Object.keys(LPT_TYPE).map(lpt => (
         <MatterTokenManageModal key={lpt} lptType={LPT_TYPE[lpt as keyof typeof LPT_TYPE]} />
       ))}
-    </AutoColumn>
+    </Wrapper>
   )
 }
