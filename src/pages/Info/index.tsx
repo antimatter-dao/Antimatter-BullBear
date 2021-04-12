024483c4e1579db73d5b30697e194430d21c2d3a
TengYingWei <734970600@qq.com>
2021-04-13 02:11:52 +0800

    Delete tvl of info

--- src/pages/Info/index.tsx
+++ src/pages/Info/index.tsx
@@ -1,19 +1,15 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import AppBody from 'pages/AppBody'
 import { AutoRow, RowBetween } from 'components/Row'
 import { TYPE } from 'theme'
 import { AutoColumn } from 'components/Column'
-import { TranslucentCard } from 'components/Card'
-import useTheme from 'hooks/useTheme'
-import antimatterLogo from 'assets/svg/antimatter_background_logo.svg'
-import { useAllOptionTypes, useValues } from '../../state/market/hooks'
-import { ChainId, JSBI, WETH } from '@uniswap/sdk'
+//import antimatterLogo from 'assets/svg/antimatter_background_logo.svg'
+import { useAllOptionTypes } from '../../state/market/hooks'
+import { ChainId, WETH } from '@uniswap/sdk'
 import { parseBalance } from '../../utils/marketStrategyUtils'
-import { CountUp } from 'use-count-up/lib'
 import PriceItem from './PriceItem'
 import AssetItem from './AssetItem'
-import { USDT } from '../../constants'
 
 export function NumberWithUnit({ unit, number }: { unit: string; number: string }) {
   return (
@@ -30,48 +26,48 @@ export const Divider = styled.div`
 `
 
 export default function Info() {
-  const theme = useTheme()
-  const values = useValues()
+  // const theme = useTheme()
+  // const values = useValues()
   const optionTypes = useAllOptionTypes()
-  const allValue = useMemo(() => {
-    if (!values || values.length === 0) return undefined
-    return values?.reduce((pre, cur) => {
-      return {
-        priceUnderlying: JSBI.add(JSBI.BigInt(pre.priceUnderlying), JSBI.BigInt(cur.priceUnderlying)).toString(),
-        valueReserve: JSBI.add(JSBI.BigInt(pre.valueReserve), JSBI.BigInt(cur.valueReserve)).toString()
-      }
-    })
-  }, [values])
+  // const allValue = useMemo(() => {
+  //   if (!values || values.length === 0) return undefined
+  //   return values?.reduce((pre, cur) => {
+  //     return {
+  //       priceUnderlying: JSBI.add(JSBI.BigInt(pre.priceUnderlying), JSBI.BigInt(cur.priceUnderlying)).toString(),
+  //       valueReserve: JSBI.add(JSBI.BigInt(pre.valueReserve), JSBI.BigInt(cur.valueReserve)).toString()
+  //     }
+  //   })
+  // }, [values])
 
-  const countUpAmountPrevious = '0'
+  // const countUpAmountPrevious = '0'
   return (
     <AppBody style={{ maxWidth: '640px' }}>
       <AutoRow justify="center">
         <TYPE.mediumHeader>Antimatter Info</TYPE.mediumHeader>
       </AutoRow>
       <AutoColumn gap="10px" style={{ marginTop: '20px' }}>
-        <TranslucentCard style={{ position: 'relative', overflow: 'hidden' }}>
-          <img
-            src={antimatterLogo}
-            style={{ position: 'absolute', right: '40px', top: '24px', height: '75px' }}
-            alt=""
-          />
-          <AutoColumn gap="16px">
-            <TYPE.smallGray>TOTAL VALUE LOCKED</TYPE.smallGray>
-            <TYPE.main fontWeight={500} fontSize={40} color={theme.primary1}>
-              $
-              <CountUp
-                key={parseBalance({ val: allValue?.valueReserve, token: WETH[ChainId.MAINNET] })}
-                isCounting
-                decimalPlaces={2}
-                start={parseFloat(countUpAmountPrevious)}
-                end={parseFloat(parseBalance({ val: allValue?.valueReserve ?? '0', token: USDT }))}
-                thousandsSeparator={','}
-                duration={1}
-              />
-            </TYPE.main>
-          </AutoColumn>
-        </TranslucentCard>
+        {/*<TranslucentCard style={{ position: 'relative', overflow: 'hidden' }}>*/}
+        {/*  <img*/}
+        {/*    src={antimatterLogo}*/}
+        {/*    style={{ position: 'absolute', right: '40px', top: '24px', height: '75px' }}*/}
+        {/*    alt=""*/}
+        {/*  />*/}
+        {/*  <AutoColumn gap="16px">*/}
+        {/*    <TYPE.smallGray>TOTAL VALUE LOCKED</TYPE.smallGray>*/}
+        {/*    <TYPE.main fontWeight={500} fontSize={40} color={theme.primary1}>*/}
+        {/*      $*/}
+        {/*      <CountUp*/}
+        {/*        key={parseBalance({ val: allValue?.valueReserve, token: WETH[ChainId.MAINNET] })}*/}
+        {/*        isCounting*/}
+        {/*        decimalPlaces={2}*/}
+        {/*        start={parseFloat(countUpAmountPrevious)}*/}
+        {/*        end={parseFloat(parseBalance({ val: allValue?.valueReserve ?? '0', token: USDT }))}*/}
+        {/*        thousandsSeparator={','}*/}
+        {/*        duration={1}*/}
+        {/*      />*/}
+        {/*    </TYPE.main>*/}
+        {/*  </AutoColumn>*/}
+        {/*</TranslucentCard>*/}
 
         {optionTypes.map(item => {
           return (
