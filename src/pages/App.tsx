6fb16eb39e5ba0fdeac8933ea873bf02d9a61ca4
iv172 <iv172m@hotmail.com>
2021-04-19 18:10:55 +0800

    add warning modal

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -39,6 +39,7 @@ import Info from './Info'
 import MatterRedemption from './MatterToken/MatterRedemption'
 import WelcomeSlider from 'components/WelcomeSlider'
 import FAQ from './FAQ'
+import WarningModal from 'components/Modal/WarningModal'
 
 const AppWrapper = styled.div`
   display: flex;
@@ -111,6 +112,7 @@ export default function App() {
             <Popups />
             <Polling />
             <WelcomeSlider />
+            <WarningModal />
             {/* <TopLevelModals /> */}
             <Web3ReactManager>
               <Switch>
