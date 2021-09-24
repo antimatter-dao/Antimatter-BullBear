cb5349f795c024412ee4fa497df91a8a5a01739f
iv172 <iv172@hotmail.com>
2021-09-24 11:46:57 +0800

    fix calculator error display

--- src/pages/Calculator/index.tsx
+++ src/pages/Calculator/index.tsx
@@ -29,10 +29,11 @@ enum ERROR {
   EMPTY_PRICE = 'Price cannot be empty or 0',
   EMPTY_PRICE_CAP = 'Price ceiling cannot be empty or 0',
   EMPTY_PRICE_FLOOR = 'Price floor cannot be empty or 0',
-  EMPTY_TOTAL_CALL = 'Call Issuance be empty or 0',
-  EMPTY_TOTAL_PUT = 'Put Issuance be empty or 0',
-  LARGER_FLOOR_THAN_CAP = 'Price Floor cannot be larger than price ceiling',
-  PRICE_EXCEEDS_PRICE_RANGE = 'Price must be between price floor and price ceiling'
+  EMPTY_TOTAL_CALL = 'Call issuance cannot be empty or 0',
+  EMPTY_TOTAL_PUT = 'Put issuance cannot be empty or 0',
+  LARGER_FLOOR_THAN_CAP = 'Price floor cannot be larger than price ceiling',
+  PRICE_EXCEEDS_PRICE_RANGE = 'Price must be between price floor and price ceiling',
+  ISSUANCE_ZERO = 'Call issuance and put issuance cannont be 0 simultaneously'
 }
 
 export default function Calculator() {
@@ -55,10 +56,11 @@ export default function Calculator() {
       return
     }
     let error = ''
+    if (+totalPut === 0 && +totalCall === 0) error = ERROR.ISSUANCE_ZERO
     if (+price < +priceFloor || +price > +priceCap) error = ERROR.PRICE_EXCEEDS_PRICE_RANGE
     if (+priceFloor > +priceCap) error = ERROR.LARGER_FLOOR_THAN_CAP
-    if (!totalPut || +totalPut === 0) error = ERROR.EMPTY_TOTAL_PUT
-    if (!totalCall || +totalCall === 0) error = ERROR.EMPTY_TOTAL_CALL
+    if (!totalPut) error = ERROR.EMPTY_TOTAL_PUT
+    if (!totalCall) error = ERROR.EMPTY_TOTAL_CALL
     if (!priceFloor || +priceFloor === 0) error = ERROR.EMPTY_PRICE_FLOOR
     if (!priceCap || +priceCap === 0) error = ERROR.EMPTY_PRICE_CAP
     if (!price || +price === 0) error = ERROR.EMPTY_PRICE
