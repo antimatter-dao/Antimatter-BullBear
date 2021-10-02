a21cde02287f72875ff1a2b76336116dcd3b2386
TengYingWei <734970600@qq.com>
2021-10-03 04:12:16 +0800

    Improve Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -272,7 +272,6 @@ const AccountElement = styled.div<{ active: boolean }>`
   white-space: nowrap;
   padding: ${({ active }) => (active ? '14px 16px' : 'unset')};
   padding-right: 0;
-  height: 44px;
   ${({ theme }) => theme.mediaWidth.upToExtraSmall`
   width:100%`}
 `
